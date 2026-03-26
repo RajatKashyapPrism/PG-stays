5dbbc3fd0d1078c01fc27717d11d08456e024ad4
rajat18051991 <rajat.kshyp@yahoo.com>
2026-03-26 17:06:13 +0530

    chnage dthe config to static site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
-import keystatic from '@keystatic/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: node({ mode: 'middleware' }),
-  integrations: [react(), keystatic()],
+  output: 'static',
+  integrations: [react()],
 });
